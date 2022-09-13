71004857289f1bd6303b634b2659592dee2d116a
Relieveddd <457425537@qq.com>
2022-09-13 22:20:18 +0800

    <input         type="text"         autocomplete="nope"         class="proc-unit-textbox-50 Inner50"         placeholder="Search"         v-model="keyword"       />               <a           href="javascript:void(0);"           @click="handleTableDetail(jsonItemLoop.StrPK010)"           >编辑</a         >         import { onMounted, watch, ref, getCurrentInstance } from "vue";         //创建防抖搜索需要用的变量 let keyword = ref("");   //创建变量用于接收响应的数据   let reqParam = {Keyword: "",   };   if (keyword.value.length > 0) {     reqParam.Keyword = keyword.value;   }   //CurrentDtaGroup:栏目号/  PageIndex：当前页数 /PageSize:当前页数   proxy.$HelperAxios     .post("/DataAccessWithGroup/QueryPagingList", reqParam)     //防抖函数 function handleDebounce(funcFollowIn, numDelayIn) {   let timerInner = null;   return (...args) => {     if (timerInner) clearTimeout(timerInner);     timerInner = setTimeout(() => {       funcFollowIn.apply(this, args);     }, numDelayIn);   }; } //实例化防抖函数 const handleSearch = handleDebounce((valIn) => {   //再次执行   jsonFuncMain(); }, 1000); //将搜索功能放在onMounted生命周期内 onMounted(() => {   watch(     () => keyword.value,     (valNew, valOld) => {       //重置分页器       jsonPaging.value.PageIndex = 1;       jsonPaging.value.NumRowQuantity = 0;       handleSearch(valNew);     }   ); }); // 点击编辑按钮跳转到文章详情页 function handleTableDetail(strParmasKeyIn) {   proxy.$router.push({     path: "/LayoutViewSystemMgt/DetailedDataTextarea",     query: { PrimaryKey: strParmasKeyIn },   }); }

--- src/views/SystemMgt/DetailedData/MainViewTextarea.vue
+++ src/views/SystemMgt/DetailedData/MainViewTextarea.vue
@@ -1,38 +1,90 @@
 ﻿<template>
+  文章详情页
   <div class="proc-unit-form-50">
     <span class="inner-title-50 proc-unit-char-02">Mark Title</span>
-    <!-- <input type="text" autocomplete="nope" class="proc-unit-textbox-50" placeholder="Mark Title" style="width: 800px" v-model="jsonMainEdited.TitleMark" /> -->
+    <input
+      type="text"
+      autocomplete="nope"
+      class="proc-unit-textbox-50"
+      placeholder="Mark Title"
+      style="width: 800px"
+      v-model="jsonMainEdited.TitleMark"
+    />
   </div>
   <div class="auxi-clear"></div>
   <div class="proc-unit-form-50">
     <span class="inner-title-50 proc-unit-char-02">Formal Title</span>
-    <!-- <input type="text" autocomplete="nope" class="proc-unit-textbox-50" placeholder="Formal Title" style="width: 800px" v-model="jsonMainEdited.TitleFormal" /> -->
+    <input
+      type="text"
+      autocomplete="nope"
+      class="proc-unit-textbox-50"
+      placeholder="Formal Title"
+      style="width: 800px"
+      v-model="jsonMainEdited.TitleFormal"
+    />
   </div>
   <div class="auxi-clear"></div>
   <div class="proc-unit-form-50">
     <span class="inner-title-50 proc-unit-char-02">Short Title</span>
-    <!-- <input type="text" autocomplete="nope" class="proc-unit-textbox-50" placeholder="Short Title" style="width: 800px" v-model="jsonMainEdited.TitleShort" /> -->
+    <input
+      type="text"
+      autocomplete="nope"
+      class="proc-unit-textbox-50"
+      placeholder="Short Title"
+      style="width: 800px"
+      v-model="jsonMainEdited.TitleShort"
+    />
   </div>
   <div class="auxi-clear"></div>
   <div class="proc-unit-form-52">
     <span class="inner-title-50 proc-unit-char-02">导读</span>
-    <!-- <textarea class="proc-unit-textbox-50" style="width: 800px; height: 80px" v-model="jsonMainEdited.Intro"></textarea> -->
+    <textarea
+      class="proc-unit-textbox-50"
+      style="width: 800px; height: 80px"
+      v-model="jsonMainEdited.Intro"
+    ></textarea>
   </div>
   <div class="auxi-clear"></div>
   <div class="auxi-clear"></div>
   <div class="proc-unit-form-50">
     <span class="inner-title-50 proc-unit-char-02">Take Effect</span>
-    <!-- TODO 在此使用 el-select 渲染下拉选项. -->
+    <!-- TODO 在此/使用 el-select 渲染下拉选项. -->
+    <el-select
+      v-model="jsonMainEdited.NumTE030"
+      class="m-2"
+      placeholder="Select"
+      size="large"
+    >
+      <el-option
+        v-for="item in jsonArrayoptions"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      />
+    </el-select>
   </div>
   <div class="auxi-clear"></div>
   <div class="proc-unit-form-50">
     <span class="inner-title-50 proc-unit-char-02">Keyword</span>
-    <!-- <input type="text" autocomplete="nope" class="proc-unit-textbox-50" placeholder="Keyword" style="width: 800px" v-model="jsonMainEdited.Keyword" /> -->
+    <input
+      type="text"
+      autocomplete="nope"
+      class="proc-unit-textbox-50"
+      placeholder="Keyword"
+      style="width: 800px"
+      v-model="jsonMainEdited.Keyword"
+    />
   </div>
   <div class="auxi-clear"></div>
   <div class="proc-unit-form-50">
     <span class="inner-title-50 proc-unit-char-02">Source</span>
-    <!-- <input type="text" autocomplete="nope" class="proc-unit-textbox-50" placeholder="Source" v-model="jsonMainEdited.StrSource" /> -->
+    <input
+      type="text"
+      autocomplete="nope"
+      class="proc-unit-textbox-50"
+      placeholder="Source"
+      v-model="jsonMainEdited.StrSource"
+    />
   </div>
   <div class="auxi-clear"></div>
   <div class="ProcUnitHorizBar3W AM04Bottom3R">
@@ -41,19 +93,62 @@
     </div>
     <div>
       <a class="InnerBtn3R" title="重置">重置</a>
-      <a class="InnerBtn3R" ei020="FileList" eg010="1" title="文件管理">文件管理</a>
-      <a class="InnerBtn3R" title="提交">提交</a>
+      <a class="InnerBtn3R" ei020="FileList" eg010="1" title="文件管理"
+        >文件管理</a
+      >
+      <a class="InnerBtn3R" title="提交" @click="reqFuncMain">提交</a>
     </div>
   </div>
 </template>
 <script setup>
-onBeforeMount(() => {
-});
-onMounted(() => {
-});
-onActivated(() => {
-});
+import { ref, getCurrentInstance, watch, onMounted, onActivated } from "vue";
+import { ElMessageBox, ElMessage } from "element-plus";
+const { proxy } = getCurrentInstance();
+//从sessionStorage获取栏目数据,绑定到组件上
+const jsonCurrntDataGroup = JSON.parse(
+  window.sessionStorage.getItem("jsonCurrentDataGroup")
+);
+let jsonMainEdited = ref({});
+//申请变量保存Param请求，之后proxy请求数据的时候直接使用变量名
+let reqParam = {
+  CurrentDataGroup: jsonCurrntDataGroup.NumDataGroup,
+  StrPrimaryKey: proxy.$route.query.PrimaryKey,
+};
+// 发送请求数据
+proxy.$HelperAxios
+  .post("/DataAccessWithGroup/DetailedDataRow", reqParam)
+  .then((responseResult) => {
+    jsonMainEdited.value = responseResult.Payload;
+  });
+
+// 工具函数---点击事件提交修改文章请求
+function reqFuncMain() {
+  let reqParam = jsonMainEdited.value;
+  reqParam.CurrentDataGroup = jsonCurrntDataGroup.NumDataGroup;
+  reqParam.StrPrimaryKey = proxy.$route.query.PrimaryKey;
+  // 发送请求数据
+  proxy.$HelperAxios
+    .post("/DataAccessWithGroup/UpdateDataRow", reqParam)
+    .then((responseResult) => {
+      ElMessage({
+        type: responseResult.CT040,
+        message: "文章修改完成",
+      });
+    });
+}
+//el-option是否生效选择数据的数组
+const jsonArrayoptions = [
+  {
+    value: 0,
+    label: "失效",
+  },
+  {
+    value: 1,
+    label: "生效",
+  },
+];
+onMounted(() => {});
+onActivated(() => {});
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
