8d7e0cbcd928137cb3e08c2c1325ebbf26f2f651
Relieveddd <457425537@qq.com>
2022-09-13 22:26:33 +0800

    <input         type="text"         autocomplete="nope"         class="proc-unit-textbox-50 Inner50"         placeholder="Search"         v-model="keyword"       />       <a           href="javascript:void(0);"           @click="handleTableDetail(jsonItemLoop.StrPK010)"           >编辑</a         >         import { onMounted, watch, ref, getCurrentInstance } from "vue";         //创建防抖搜索需要用的变量 let keyword = ref("");   //创建变量用于接收响应的数据   let reqParam = {      Keyword: "",   };   if (keyword.value.length > 0) {     reqParam.Keyword = keyword.value;   }   //CurrentDtaGroup:栏目号/  PageIndex：当前页数 /PageSize:当前页数   proxy.$HelperAxios     .post("/DataAccessWithGroup/QueryPagingList", reqParam)     //防抖函数 function handleDebounce(funcFollowIn, numDelayIn) {   let timerInner = null;   return (...args) => {     if (timerInner) clearTimeout(timerInner);     timerInner = setTimeout(() => {       funcFollowIn.apply(this, args);     }, numDelayIn);   }; } //实例化防抖函数 const handleSearch = handleDebounce((valIn) => {   //再次执行   jsonFuncMain(); }, 1000); //将搜索功能放在onMounted生命周期内 onMounted(() => {   watch(     () => keyword.value,     (valNew, valOld) => {       //重置分页器       jsonPaging.value.PageIndex = 1;       jsonPaging.value.NumRowQuantity = 0;       handleSearch(valNew);     }   ); }); // 点击编辑按钮跳转到文章详情页 function handleTableDetail(strParmasKeyIn) {   proxy.$router.push({     path: "/LayoutViewSystemMgt/DetailedDataTextarea",     query: { PrimaryKey: strParmasKeyIn },   }); }

--- src/views/SystemMgt/PagingList/MainViewUniv.vue
+++ src/views/SystemMgt/PagingList/MainViewUniv.vue
@@ -1,7 +1,13 @@
 ﻿<template>
   <div class="ProcUnitHorizMenu50">
     <div class="Inner50">
-      <!-- <input type="text" autocomplete="nope" class="proc-unit-textbox-50 Inner50" placeholder="Search" v-model="keyword" /> -->
+      <input
+        type="text"
+        autocomplete="nope"
+        class="proc-unit-textbox-50 Inner50"
+        placeholder="Search"
+        v-model="keyword"
+      />
     </div>
   </div>
 
@@ -23,7 +29,11 @@
       <td>{{ jsonItemLoop.NumAL040 }}</td>
       <td>{{ jsonItemLoop.NumTE030 }}</td>
       <td class="InnerCP0250 proc-unit-char10">
-        <a href="javascript:void(0);">编辑</a>
+        <a
+          href="javascript:void(0);"
+          @click="handleTableDetail(jsonItemLoop.StrPK010)"
+          >编辑</a
+        >
         <a href="javascript:void(0);">删除</a>
       </td>
     </tr>
@@ -48,13 +58,16 @@
   </div>
 </template>
 <script setup>
-import { ref, getCurrentInstance } from "vue";
+import { onMounted, watch, ref, getCurrentInstance } from "vue";
 const { proxy } = getCurrentInstance();
 let jsonPaging = ref({
   NumRowQuantity: 0,
   PageSize: 15,
   PageIndex: 1,
 });
+//创建防抖搜索需要用的变量
+let keyword = ref("");
+
 let jsonArrayMain = ref([]);
 function setFuncPaging(_numPageIndex) {
   jsonPaging.value.PageIndex = _numPageIndex;
@@ -65,15 +78,21 @@ const jsonCurrentDataGroup = JSON.parse(
 );
 //工具函数用于请求文章列表
 function jsonFuncMain() {
+  //创建变量用于接收响应的数据
+  let reqParam = {
+    CurrentDataGroup: jsonCurrentDataGroup.NumDataGroup,
+    PageIndex: jsonPaging.value.PageIndex,
+    PageSize: jsonPaging.value.PageSize,
+    Keyword: "",
+  };
+  if (keyword.value.length > 0) {
+    reqParam.Keyword = keyword.value;
+  }
   //CurrentDtaGroup:栏目号/  PageIndex：当前页数 /PageSize:当前页数
   proxy.$HelperAxios
-    .post("/DataAccessWithGroup/QueryPagingList", {
-      CurrentDataGroup: jsonCurrentDataGroup.NumDataGroup,
-      PageIndex: jsonPaging.value.PageIndex,
-      PageSize: jsonPaging.value.PageSize,
-    })
+    .post("/DataAccessWithGroup/QueryPagingList", reqParam)
     .then((responseResult) => {
-      // console.log(responseResult);
+      // console.log(responseResult.Payload.NumRowQuantity);
       //获取栏目数据，赋值给el-tree
       //文库el-pagnation组件提供总数居条数
       jsonPaging.value.NumRowQuantity = responseResult.Payload.NumRowQuantity;
@@ -82,7 +101,42 @@ function jsonFuncMain() {
       // console.log(jsonArrayMain.value);
     });
 }
+
 jsonFuncMain();
+//防抖函数
+function handleDebounce(funcFollowIn, numDelayIn) {
+  let timerInner = null;
+  return (...args) => {
+    if (timerInner) clearTimeout(timerInner);
+    timerInner = setTimeout(() => {
+      funcFollowIn.apply(this, args);
+    }, numDelayIn);
+  };
+}
+//实例化防抖函数
+const handleSearch = handleDebounce((valIn) => {
+  //再次执行
+  jsonFuncMain();
+}, 1000);
+//将搜索功能放在onMounted生命周期内
+onMounted(() => {
+  watch(
+    () => keyword.value,
+    (valNew, valOld) => {
+      //重置分页器
+      jsonPaging.value.PageIndex = 1;
+      jsonPaging.value.NumRowQuantity = 0;
+      handleSearch(valNew);
+    }
+  );
+});
+// 点击编辑按钮跳转到文章详情页
+function handleTableDetail(strParmasKeyIn) {
+  proxy.$router.push({
+    path: "/LayoutViewSystemMgt/DetailedDataTextarea",
+    query: { PrimaryKey: strParmasKeyIn },
+  });
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
